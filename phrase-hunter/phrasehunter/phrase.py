8d42c16a62ea5cb2da9ff1bb08ba5b2735a94f9e
James-Rouse <woootlooops@gmail.com>
2021-05-21 17:59:09 -0400

    Phrase class tentatively finished

--- phrase-hunter/phrasehunter/phrase.py
+++ phrase-hunter/phrasehunter/phrase.py
@@ -1,38 +1,53 @@
-# Create your Phrase class logic here.
+"""Used to pick a random phrase."""
+import random
+
+
 class Phrase:
+    """Deals with phrase manipulation for the game."""
 
-    phrases = [
-    'blowing away',
-    'in the wind', 
-    'the brown hat', 
-    'my English teacher', 
-    'the grocery store', 
-    'ran quickly', 
-    'has been raining', 
-    'on the boat', 
-    'above the stove', 
-    'around the corner',
-    'hello world']
+    def random_phrase():
+        """Choose a random phrase."""
+        phrases = [
+        'blowing away',
+        'in the wind',
+        'the brown hat',
+        'my English teacher',
+        'the grocery store',
+        'ran quickly',
+        'has been raining',
+        'on the boat',
+        'above the stove',
+        'around the corner',
+        'hello world']
+        return random.choice(phrases)
 
-    def __init__(self, phrase):
-        self.phrase = phrase.lower()
+    def __init__(self, chosen_phrase):
+        """Recieve phrase for use in game."""
+        self.phrase = chosen_phrase.lower()
 
-    def display(self):
+    def display(self, guessed_letter):
+        """Print the phrase to console with only guessed letters visibile."""
         hidden_phrase = self.phrase
         for letter in self.phrase:
             if letter != ' ':
                 if letter != guessed_letter:
                     hidden_phrase = hidden_phrase.replace(letter, '_')
-        print(hidden_phrase)
+        return hidden_phrase
 
+    def check_letter(self, guessed_letter):
+        """Check to see if the letter selected by the user matches a letter in the phrase."""
+        return guessed_letter in self.phrase
 
-#    def check_letter():
+    def check_complete(self, hidden_phrase):
+        """Check to see if the whole phrase has been guessed."""
+        return hidden_phrase == self.phrase
 
-#    def check_complete():
-
-
-phrase_instance_1 = Phrase('hello world')
 
 guessed_letter = 'o'
+chosen_phrase = Phrase.random_phrase()
+phrase_one = Phrase('hello world')
+hidden_phrase = phrase_one.display(guessed_letter)
 
-phrase_instance_1.display()
+print(phrase_one.display(guessed_letter))
+print(phrase_one.check_letter(guessed_letter))
+print(phrase_one.check_complete(hidden_phrase))
